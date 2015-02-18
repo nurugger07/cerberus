d7605025c974bff3e01836ce5c7ebbca44824f1c
Hashrocket Workstation <dev@hashrocket.com>
2015-02-18 12:45:34 -0500

    Tokenize basic xml document

---
+++ test/cerberus/tokenizer_test.exs
@@ -0,0 +1,31 @@
+defmodule Cerberus.TokenizeTest do
+  use ExUnit.Case
+
+  import Cerberus.Tokenize
+
+  @xml ~s{
+    <?xml version="1.0"?>
+    <!DOCTYPE book SYSTEM "book.dtd">
+    <book id="bk101">
+      <author>Gambardella, Matthew</author>
+      <price>44.95</price>
+      <publish_date>2012-12-21</publish_date>
+    </book>
+  }
+
+  @tokens [
+    {:header, "<?xml version=\"1.0\"?>"},
+    {:doctype, "<!DOCTYPE book SYSTEM \"book.dtd\">"},
+    {:start_node, "book", "<book id=\"bk101\">"},
+    {:attribute, "id", "bk101", "<book id=\"bk101\">"},
+    {:element, "author", "Gambardella, Matthew", "<author>Gambardella, Matthew</author>"},
+    {:element, "price", "44.95", "<price>44.95</price>" },
+    {:element, "publish_date", "2012-12-21", "<publish_date>2012-12-21</publish_date>"},
+    {:end_node, "book", "</book>"}
+  ]
+
+  test "tokenize XML" do
+    assert @tokens == (tokenize @xml)
+  end
+
+end
--- test/cerberus_test.exs
+++ test/cerberus_test.exs
@@ -1,10 +1,3 @@
-defmodule Cerberus.Tokenize do
-  def tokenize(xml) do
-    xml
-  end
-
-end
-
 defmodule Cerberus.Parser do
   def parse(tokens) do
     tokens
@@ -23,35 +16,9 @@ defmodule Cerberus.Render do
   import Cerberus.Compiler
 
   def render(xml, :json) do
-    xml |> tokenize |> parse |> compile(:json)
+    xml |> compile(:json)
+    # xml |> tokenize |> parse |> compile(:json)
   end
-end
-
-defmodule Cerberus.TokenizeTest do
-  use ExUnit.Case
-
-  import Cerberus.Tokenize
-
-  @xml Regex.replace(~r/(^\s*)|(\s+$)|(\n)/m, ~s{
-    <?xml version="1.0"?>
-    <book id="bk101">
-      <author>Gambardella, Matthew</author>
-      <title>XML Developer's Guide</title>
-      <genre>Computer</genre>
-      <price>44.95</price>
-      <publish_date>2000-10-01</publish_date>
-      <description>An in-depth look at creating applications with XML.</description>
-    </book>
-  }
-
-  @tokens [
-    [ element: "book", attributes
-  ]
-
-  test "tokenize XML" do
-
-  end
-
 end
 
 defmodule CerberusTest do
